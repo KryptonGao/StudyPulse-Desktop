f2fb789dc69da28501a73444da287a9a36da1e56
ChenkaiGao <DevChenkai@outlook.com>
2026-07-30 01:00:46 +0800

    Initial Commit

---
+++ StudyPulse Desktop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyPulse Desktop
+//
+//  Created by Chenkai Gao on 2026/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyPulse DesktopTests/StudyPulse_DesktopTests.swift
@@ -0,0 +1,19 @@
+//
+//  StudyPulse_DesktopTests.swift
+//  StudyPulse DesktopTests
+//
+//  Created by Chenkai Gao on 2026/7/30.
+//
+
+import Testing
+@testable import StudyPulse_Desktop
+
+struct StudyPulse_DesktopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
